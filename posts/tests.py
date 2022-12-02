ac2ce1431846454b390889f908ed08841d712055
DOdrums <dirkornee@hotmail.com>
2022-12-02 16:19:04 +0000

    Add tests for the PostDetailView

--- posts/tests.py
+++ posts/tests.py
@@ -25,3 +25,38 @@ class PostListViewTests(APITestCase):
     def test_logged_out_user_cannot_creat_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='adam', password='pass')
+        User.objects.create_user(username='brian', password='pass')
+
+    def test_can_retrieve_post_using_valid_id(self):
+        adam = User.objects.get(username='adam')
+        Post.objects.create(owner=adam, title='a title')
+
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'a title')
+
+    def test_can_not_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        adam = User.objects.get(username='adam')
+        Post.objects.create(owner=adam, title='a title')
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title', 'owner': adam.id})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_can_update_own_post(self):
+        adam = User.objects.get(username='adam')
+        brian = User.objects.get(username='brian')
+        Post.objects.create(owner=adam, title='a title')
+        self.client.login(username='brian', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title', 'owner': adam.id})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
