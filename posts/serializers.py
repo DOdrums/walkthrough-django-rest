a56e050dfaf75daf0878ed44c1f35899341e828f
DOdrums <dirkornee@hotmail.com>
2022-12-05 15:22:31 +0000

    Add method fields to serializers for followed_id and liked_id

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
+
 
 class PostSerializer(serializers.ModelSerializer):
     owner: serializers.ReadOnlyField(source='owner.username')
@@ -10,6 +12,7 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(
         source='owner.profile.image.url'
         )
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -31,11 +34,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
             'title', 'content', 'image', 'is_owner',
-            'profile_id', 'profile_image', 'image_filter'
+            'profile_id', 'profile_image', 'image_filter', 'like_id',
         ]
 
