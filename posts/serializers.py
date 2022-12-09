154114b4fce9572d4627c5505649fa599b0fa9be
DOdrums <dirkornee@hotmail.com>
2022-12-09 13:48:51 +0000

    Update serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,7 +4,7 @@ from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
-    owner: serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(
         source='owner.profile.id'
