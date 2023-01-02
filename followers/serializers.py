879bd8de95b5bb67a8b550aa97f6b0a1a4fd9e0a
DOdrums <dirkornee@hotmail.com>
2023-01-02 12:01:21 +0000

    Copy code over from source, to fix follower bug

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,6 +1,6 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Follower 
+from .models import Follower
 
 
 class FollowerSerializer(serializers.ModelSerializer):
@@ -11,17 +11,14 @@ class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     followed_name = serializers.ReadOnlyField(source='followed.username')
 
-
     class Meta:
-        model = Follower 
+        model = Follower
         fields = [
-            'id', 'owner', 'followed', 'followed_name', 'created_at', 
+            'id', 'owner', 'created_at', 'followed', 'followed_name'
         ]
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({
-                'detail': 'possible duplicate'
-            }) 
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
