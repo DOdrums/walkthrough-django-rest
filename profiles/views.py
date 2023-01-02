1da80aea4d0226c62ca2b75cd830be636430523e
DOdrums <dirkornee@hotmail.com>
2023-01-02 12:18:57 +0000

    Copy more code over for bugfix

--- profiles/views.py
+++ profiles/views.py
@@ -21,16 +21,16 @@ class ProfileList(generics.ListAPIView):
         filters.OrderingFilter,
         DjangoFilterBackend,
     ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
     ordering_fields = [
-        'post_count',
+        'posts_count',
         'followers_count',
         'following_count',
         'owner__following__created_at',
         'owner__followed__created_at',
-    ]
-    filterset_fields = [
-        'owner__following__followed__profile',
-        'owner__followed__owner__profile',
     ]
 
 
@@ -39,9 +39,9 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = ProfileSerializer
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
+    serializer_class = ProfileSerializer
